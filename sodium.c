afe2f6b369970650c17a4d3d06be9149c8a9641b
Andrew Clayton <andrew@digital-domain.net>
2008-05-01 18:51:25 +0100

    [sodium] Get basic viewing of images working.
    Load images from directory into a static array. Allow for scrolling forwards &
    backwards through the images with the cursor keys (Up & down) and q to quit.

--- sodium.c
+++ sodium.c
@@ -7,20 +7,24 @@
 #include <glib/gstdio.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <string.h>
 
-
+/* Image grid dimentions */
 #define GRID_SIZE	9
 #define ROW_SIZE	3
 #define COL_SIZE	3
 
+/* Direction of scrolling */
+#define FWD		0
+#define BWD		1
 
-DIR *dir;
-struct dirent *entry;
-int ao = 0;
 
-/* Doubly linked-list of image filenames */
-GList *images = NULL;
+/* Array to hold image filenames */
+char files[500][255];
+int array_pos = 0;
+int nfiles = 0;
 
+/* Check to see if the file is a valid image */
 static gboolean
 is_supported_img(const char *name)
 {
@@ -32,61 +36,94 @@ is_supported_img(const char *name)
 	return FALSE;
 }
 
+/* Read in the image file names into a linked-list */
 static void
-process_directory(const gchar *name, ClutterActor *stage)
+process_directory(const gchar *name)
 {
-	ClutterActor *img;
-	GdkPixbuf *pixbuf;
+	DIR *dir;
+	struct dirent *entry;
 
-	int x = 0, y = 0, c = 0, r = 0;
-
-	if (!ao) {
-		dir = opendir(name);
-		printf("Opening directory: %s\n", name);
-		chdir(name);
-		ao = 1;
-	}	
+	dir = opendir(name);
+	printf("Opening directory: %s\n", name);
+	chdir(name);
 	
 	while ((entry = readdir(dir)) != NULL) {
 		if (is_supported_img(entry->d_name)) {
-			images = g_list_append(images, entry->d_name);
-			printf("Loading image: %s\n", entry->d_name);
-			pixbuf = gdk_pixbuf_new_from_file_at_size(entry->d_name,								300, 300, NULL);
-			img = clutter_texture_new_from_pixbuf(pixbuf);
-			clutter_actor_set_position(img, x, y);
-			clutter_group_add(CLUTTER_GROUP(stage), img);
-			clutter_actor_show(img);
-			
-			c += 1;
-			if (c == COL_SIZE) {
-				x = 0;
-				y += 300;
-				c = 0;
-				r += 1;
-			} else 
-				x += 300;
-
-			printf("c = %d, r = %d, x = %d, y = %d\n", c, r, x, y);
-			
-			if (r == ROW_SIZE) 
-				break;
+			printf("Adding image %s to list\n", entry->d_name);
+			strncpy(files[nfiles], entry->d_name, 254);
+			nfiles++;		
 		}
     	}
 }
 
+/* Load images onto stage */
 static void
-key_release_cb (ClutterActor *stage, ClutterKeyEvent *kev, gpointer user_data)
+load_images(ClutterActor *stage, int direction)
+{
+        ClutterActor *img;
+        GdkPixbuf *pixbuf;
+	int i; 
+        int x = 0, y = 0, c = 0, r = 0;
+
+	if (direction == BWD) {
+		if (array_pos < (GRID_SIZE * 2))
+			return;
+		else
+			array_pos -= (GRID_SIZE * 2);
+	} else {
+		if (array_pos >= nfiles)
+			return;
+	}
+
+	for (i = array_pos; i < (array_pos + 9); i++) {
+		printf("Loading image: %s\n", files[i]);
+		pixbuf = gdk_pixbuf_new_from_file_at_size(files[i],  
+								300, 300, NULL);
+		img = clutter_texture_new_from_pixbuf(pixbuf);
+		clutter_actor_set_position(img, x, y);
+		clutter_group_add(CLUTTER_GROUP(stage), img);
+		clutter_actor_show(img);
+
+		c += 1;
+		if (c == COL_SIZE) {
+			x = 0;
+			y += 300;
+			c = 0;
+			r += 1;
+		} else
+			x += 300;
+
+		array_pos++;
+		/*printf("c = %d, r = %d, x = %d, y = %d, array_pos = %d\n", 
+						c, r, x, y, array_pos);*/
+
+		if (r == ROW_SIZE)
+			break;
+        }
+}
+
+/* Process key events */
+static void
+key_release_cb(ClutterActor *stage, ClutterKeyEvent *kev, gpointer user_data)
 {
 	switch (clutter_key_event_symbol(kev)) {
 	case CLUTTER_Down:
 		printf("Got key event: %d\n", clutter_key_event_symbol(kev));
-		process_directory(NULL, stage);
+		load_images(stage, FWD);
+		break;
+	case CLUTTER_Up:
+		printf("Got key event: %d\n", clutter_key_event_symbol(kev));
+		load_images(stage, BWD);
+		break;
+	case CLUTTER_q:
+		printf("Got key event: %d\n", clutter_key_event_symbol(kev));
+		clutter_main_quit ();
 		break;
 	}
 }
 
 int
-main (int argc, char *argv[])
+main(int argc, char *argv[])
 {
 	ClutterActor *stage;
 	ClutterColor stage_clr = { 0x00, 0x00, 0x00, 0xff };
@@ -103,7 +140,9 @@ main (int argc, char *argv[])
 	clutter_stage_set_color(CLUTTER_STAGE (stage), &stage_clr);
 
 	clutter_actor_show_all(stage);
-	process_directory(argv[1], stage);
+
+	process_directory(argv[1]);
+	load_images(stage, FWD);
 
 	g_signal_connect(stage, "key-release-event", 
 				G_CALLBACK (key_release_cb), NULL);

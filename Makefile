97c8ee448cbfc5572eb3fad1a1b43224a690d22b
Andrew Clayton <andrew@digital-domain.net>
2011-08-20 15:58:22 +0100

    Code re-org and cleanups
    - Move fucntions around in sodium.c in order to get rid of sodium.h.
    - Lots of white space fix's
    
    Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 LIBS=`pkg-config --libs clutter-1.0 glib-2.0`
 INCS=`pkg-config --cflags clutter-1.0 glib-2.0`
 
-sodium: sodium.c sodium.h
+sodium: sodium.c
 	gcc -Wall -O2 sodium.c -o sodium $(INCS) $(LIBS)
 	gzip -c sodium.1 > sodium.1.gz
 
--- sodium.c
+++ sodium.c
@@ -22,9 +22,6 @@
 #include <glib.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
 
-#include "sodium.h"
-
-
 /* Image grid dimentions */
 #define GRID_SIZE	9
 #define ROW_SIZE	3
@@ -54,6 +51,28 @@ char movie_base_path[255];	/* Path to base directory containing videos */
 int window_size; 	/* Size of the window */
 int image_size; 	/* Size of the image */
 
+/* Display a help/usage summary */
+static void display_usage()
+{
+	printf("\nUsage: sodium image_directory size [video_directory]\n\n");
+	printf("Where image_directory is the path to the location of the ");
+	printf("images.\n\n");
+	printf("size is the size of the window to display in pixels. It ");
+	printf("must be at least 300 and also be a multiple of 300.\n\n");
+	printf("video_directory is an optional directory where videos are ");
+	printf("stored. Videos listed in the movie-list file should be ");
+	printf("given relative to this path.\n");
+
+	exit(-1);
+}
+
+/* Reap child pids */
+static void reaper(int signo)
+{
+	int status;
+
+	wait(&status);
+}
 
 static gdouble on_alpha(ClutterAlpha *alpha, gpointer data)
 {
@@ -108,14 +127,14 @@ static void reset_image(ClutterActor *actor)
 static int compare_string(const void *p1, const void *p2)
 {
 	/* 
- 	 * Function used from the qsort(3) man page
- 	 *
- 	 * The actual arguments to this function are "pointers to
+	 * Function used from the qsort(3) man page
+	 *
+	 * The actual arguments to this function are "pointers to
 	 * pointers to char", but strcmp(3) arguments are "pointers
 	 * to char", hence the following cast plus dereference 
 	 */
 
-        return strcmp(* (char * const *) p1, * (char * const *) p2);
+	return strcmp(*(char * const *)p1, *(char * const *)p2);
 }
 
 /* Check to see if the file is a valid image */
@@ -127,7 +146,7 @@ static gboolean is_supported_img(const char *name)
 
 	if (fmt)
 		return (gdk_pixbuf_format_is_disabled(fmt) != TRUE);
-  
+
 	return FALSE;
 }
 
@@ -145,19 +164,196 @@ static void process_directory(const gchar *name)
 
 	printf("Opening directory: %s\n", name);
 	chdir(name);
-	
+
 	files = g_ptr_array_new();
 	while ((entry = readdir(dir)) != NULL) {
 		if (is_supported_img(entry->d_name)) {
 			fname = g_strdup(entry->d_name);
 			printf("Adding image %s to list\n", fname);
 			g_ptr_array_add(files, (gpointer)fname);
-			nfiles++;		
+			nfiles++;
 		}
     	}
 	closedir(dir);
 
 	g_ptr_array_sort(files, compare_string);
+}
+
+/* Display a "No Video" message for images with no video yet */
+static void no_video_notice(ClutterActor *stage)
+{
+	ClutterColor actor_color = { 0xff, 0xff, 0xff, 0xff };
+
+	label = clutter_text_new_full("Sans 32", "No Video", &actor_color);
+	clutter_actor_set_position(label, image_size,
+						image_size + 0.5 * image_size);
+	clutter_container_add_actor(CLUTTER_CONTAINER(stage), label);
+	clutter_actor_show(label);
+}
+
+/* fork/exec the movie */
+static void play_video(gchar **argv)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid > 0) {
+		/* parent */
+		g_strfreev(argv);
+	} else if (pid == 0) {
+		/* child */
+		execvp(argv[0], argv);
+	}
+}
+
+/* Build the command and arguments to be exec'd */
+static void build_exec_cmd(char *cmd, char *args, char *movie)
+{
+	char video_paths[1024] = "\0";
+	char buf[512] = "\0";
+	gchar **argv = NULL;
+	gchar **videos = NULL;
+
+	/* Build up a string that will be parsed into an argument list */
+	strcpy(buf, cmd);
+	strcat(buf, " ");
+	strcat(buf, args);
+	strcat(buf, " ");
+
+	/* Cater for multiple space seperated video paths to be specified */
+	if (strstr(movie, " ")) {
+		videos = g_strsplit(movie, " ", -1);
+		while (*videos != NULL) {
+			/*
+			 * Cater for either absolute or relative paths
+			 * for videos.
+			 */
+			if (*videos[0] != '/') {
+				strcat(video_paths, movie_base_path);
+				strcat(video_paths, *videos++);
+			} else {
+				strcat(video_paths, *videos++);
+			}
+
+			strcat(video_paths, " ");
+		}
+	} else {
+		if (movie[0] == '/') {
+			strcpy(video_paths, movie);
+		} else {
+			strcpy(video_paths, movie_base_path);
+			strcat(video_paths, movie);
+		}
+	}
+
+	strcat(buf, video_paths);
+	/* Split buf up into a vector array for passing to execvp */
+	g_shell_parse_argv(buf, NULL, &argv, NULL);
+
+	printf("Playing: (%s)\n", video_paths);
+	printf("execing: %s %s %s\n", cmd, args, video_paths);
+	play_video(argv);
+}
+
+/* Lookup the clicked image in the .movie-list file */
+static void lookup_video(ClutterActor *stage, char *actor)
+{
+	char string[512];
+	char image[120];
+	char movie[120];
+	char cmd[120];
+	char args[120] = "\0";
+	static FILE *fp;
+
+	printf("Opening movie list: (%s)\n", movie_list);
+	if (!(fp = fopen(movie_list, "r"))) {
+		printf("Can't open movie list: (%s)\n", movie_list);
+		exit(-1);
+	}
+
+	while (fgets(string, 512, fp)) {
+		sscanf(string, "%119[^|]|%119[^|]|%119[^|]|%119[^\n]",
+						image, movie, cmd, args);
+		if (strcmp(actor, image) == 0) {
+			fclose(fp);
+			build_exec_cmd(cmd, args, movie);
+			return;
+		}
+
+		args[0] = '\0';
+	}
+
+	/* If we get to here, we didn't find a video */
+	fclose(fp);
+	printf("No video for (%s)\n", actor);
+	no_video_notice(stage);
+}
+
+static void lookup_image(ClutterActor *stage, int img_no)
+{
+	char *image;
+
+	if (img_no <= loaded_images) {
+		image = g_ptr_array_index(files,
+					array_pos - loaded_images +
+								img_no - 1);
+		lookup_video(stage, image);
+	} else {
+		printf("No image at (%d)\n", img_no);
+	}
+}
+
+/* Determine which image on the grid was clicked. */
+static int which_image(ClutterActor *stage, int x, int y)
+{
+	int img_no = 0;
+
+	printf("array_pos = %d, loaded_images = %d\n", array_pos,
+								loaded_images);
+
+	/*
+	 * Images are layed out in a 3 x 3 grid from left to right
+	 * top to bottom. I.e image 1 is the top left image and image 9
+	 * is the bottom right image.
+	 */
+	if (x >= 0 && x <= image_size && y >= 0 && y <= image_size) {
+		img_no = 1;
+		printf("Image 1\n");
+	} else if (x >= image_size + 1 && x <= image_size * 2 &&
+						y >= 0 && y <= image_size) {
+		img_no = 2;
+		printf("Image 2\n");
+	} else if (x >= image_size * 2 + 1 && x <= image_size * 3 &&
+						y >= 0 && y <= image_size) {
+		img_no = 3;
+		printf("Image 3\n");
+	} else if (x >= 0 && x <= image_size && y >= image_size + 1 &&
+							y <= image_size * 2) {
+		img_no = 4;
+		printf("Image 4\n");
+	} else if (x >= image_size + 1 && x <= image_size * 2 &&
+				y >= image_size + 1 && y <= image_size * 2) {
+		img_no = 5;
+		printf("Image 5\n");
+	} else if (x >= image_size * 2 + 1 && x <= image_size * 3 &&
+				y >= image_size + 1 && y <= image_size * 2) {
+		img_no = 6;
+		printf("Image 6\n");
+	} else if (x >= 0 && x <= image_size && y >= image_size * 2 + 1 &&
+							y <= image_size * 3) {
+		img_no = 7;
+		printf("Image 7\n");
+	} else if (x >= image_size + 1 && x <= image_size * 2 &&
+			y >= image_size * 2 + 1 && y <= image_size * 3) {
+		img_no = 8;
+		printf("Image 8\n");
+	} else if (x >= image_size * 2 + 1 && x <= image_size * 3 &&
+			y >= image_size * 2 + 1 && y <= image_size * 3) {
+		img_no = 9;
+		printf("Image 9\n");
+	}
+
+	return img_no;
 }
 
 /* Load images onto stage */
@@ -173,25 +369,25 @@ static void load_images(ClutterActor *stage, int direction)
 
 	if (direction == BWD || direction == END) {
 		if (array_pos <= GRID_SIZE || direction == END) {
-			/* 
- 			 * If we are at the first screen of images. Loop round 
- 			 * to the end of the images. If we have exactly 
- 			 * nfiles / GRID_SIZE images then goto the end of the 
- 			 * array - GRID_SIZE images. Otherwise goto the end of 
- 			 * the array - the number of images left over below 
- 			 * GRID_SIZE.
- 			 */  
-			if (nfiles % GRID_SIZE == 0)	
+			/*
+			 * If we are at the first screen of images. Loop round
+			 * to the end of the images. If we have exactly
+			 * nfiles / GRID_SIZE images then goto the end of the
+			 * array - GRID_SIZE images. Otherwise goto the end of
+			 * the array - the number of images left over below
+			 * GRID_SIZE.
+			 */
+			if (nfiles % GRID_SIZE == 0)
 				array_pos = nfiles - GRID_SIZE;
 			else 
 				array_pos = nfiles - (nfiles % GRID_SIZE);
 		} else {
 			/*
- 			 * If we are at the end of the images and there is not
- 			 * a full grid of images to display, we need to only 
- 			 * go back GRID_SIZE + the last number of images.
- 			 * Otherwise we go back 2 * GRID_SIZE of images.
- 			 */ 	 
+			 * If we are at the end of the images and there is not
+			 * a full grid of images to display, we need to only
+			 * go back GRID_SIZE + the last number of images.
+			 * Otherwise we go back 2 * GRID_SIZE of images.
+			 */
 			if (array_pos == nfiles && nfiles % GRID_SIZE != 0)
 				array_pos -= GRID_SIZE + (nfiles % GRID_SIZE);
 			else
@@ -202,17 +398,17 @@ static void load_images(ClutterActor *stage, int direction)
 		/* Loop round to beginning of images */
 		array_pos = 0;
 	}
-	
+
 	/* How many images are on screen */	
 	loaded_images = 0;
 
-	/* 
- 	 * Remove images from the stage before loading new ones, 
- 	 * this avoids a memory leak by freeing the textures 
- 	 * and clears the stage so when there are less than
- 	 * GRID_SIZE images to view you don't get the previous 
- 	 * images still vivisble
- 	 */
+	/*
+	 * Remove images from the stage before loading new ones,
+	 * this avoids a memory leak by freeing the textures
+	 * and clears the stage so when there are less than
+	 * GRID_SIZE images to view you don't get the previous
+	 * images still vivisble.
+	 */
 	clutter_group_remove_all(CLUTTER_GROUP(stage));
 
 	for (i = array_pos; i < (array_pos + GRID_SIZE); i++) {
@@ -245,16 +441,15 @@ static void load_images(ClutterActor *stage, int direction)
 
 		array_pos++;
 		loaded_images++;
-		/*printf("c = %d, r = %d, x = %d, y = %d, array_pos = %d\n", 
+		/*printf("c = %d, r = %d, x = %d, y = %d, array_pos = %d\n",
 						c, r, x, y, array_pos);*/
         }
 }
 
 /* Process keyboard/mouse events */
-static void input_events_cb(ClutterActor *stage, ClutterEvent *event, 
+static void input_events_cb(ClutterActor *stage, ClutterEvent *event,
 							gpointer user_data)
 {
-	guint sym;
 	static int cur_toggle = 1;	/* Cursor is defaulted to visible */
 	static int pset = 0;		/* previous scroll event time */
 	int setd = 0;			/* scroll event time difference */
@@ -267,7 +462,8 @@ static void input_events_cb(ClutterActor *stage, ClutterEvent *event,
 
 	switch (event->type) {
 	case CLUTTER_KEY_PRESS: {
-		sym = clutter_event_get_key_symbol(event);
+		guint sym = clutter_event_get_key_symbol(event);
+
 		switch (sym) {
 		case CLUTTER_Page_Up:
 		case CLUTTER_Up:
@@ -285,7 +481,7 @@ static void input_events_cb(ClutterActor *stage, ClutterEvent *event,
 			break;
 		case CLUTTER_c:
 			if (!cur_toggle) {
-				g_object_set(stage, "cursor-visible", TRUE, 
+				g_object_set(stage, "cursor-visible", TRUE,
 									NULL);
 				cur_toggle = 1;
 			} else {
@@ -293,7 +489,7 @@ static void input_events_cb(ClutterActor *stage, ClutterEvent *event,
 									NULL);
 				cur_toggle = 0;
 			}
-			break;	
+			break;
 		case CLUTTER_1:
 		case CLUTTER_2:
 		case CLUTTER_3:
@@ -303,10 +499,11 @@ static void input_events_cb(ClutterActor *stage, ClutterEvent *event,
 		case CLUTTER_7:
 		case CLUTTER_8:
 		case CLUTTER_9:
-			img_no = sym - 48; /* 1 is sym(49) */ 
+			img_no = sym - 48; /* 1 is sym(49) */
 			if (img_no <= loaded_images) {
 				image = g_ptr_array_index(files,
-					array_pos - loaded_images + img_no - 1);
+					array_pos - loaded_images +
+								img_no - 1);
 				lookup_video(stage, image);
 			} else {
 				printf("No image at (%d)\n", img_no);
@@ -321,11 +518,11 @@ static void input_events_cb(ClutterActor *stage, ClutterEvent *event,
 	}
 	case CLUTTER_SCROLL:
 		/*
- 		 * Limit the amount of scroll events we act on to one event in
- 		 * SCROLL_TRESH milliseconds. This avoids over scrolling 
- 		 * through the images, particulary on a trackpad where it's 
- 		 * hard to judge what constitutes a single scroll event.
- 		 */
+		 * Limit the amount of scroll events we act on to one event in
+		 * SCROLL_TRESH milliseconds. This avoids over scrolling
+		 * through the images, particulary on a trackpad where it's
+		 * hard to judge what constitutes a single scroll event.
+		 */
 		setd = clutter_event_get_time(event) - pset;
 		printf("pset = (%d), setd = (%d)\n", pset, setd);
 
@@ -374,182 +571,6 @@ static void input_events_cb(ClutterActor *stage, ClutterEvent *event,
 	}
 }
 
-/* Determine which image on the grid was clicked. */
-static int which_image(ClutterActor *stage, int x, int y)
-{
-	int img_no = 0;
-
-	printf("array_pos = %d, loaded_images = %d\n", array_pos,
-								loaded_images);
-
-	/* 
- 	 * Images are layed out in a 3 x 3 grid from left to right
- 	 * top to bottom. I.e image 1 is the top left image and image 9
- 	 * is the bottom right image.
- 	 */
-	if (x >= 0 && x <= image_size && y >= 0 && y <= image_size) {
-		img_no = 1;
-		printf("Image 1\n");
-	} else if (x >= image_size + 1 && x <= image_size * 2 &&
-						y >= 0 && y <= image_size) {
-		img_no = 2;
-		printf("Image 2\n");
-	} else if (x >= image_size * 2 + 1 && x <= image_size * 3 &&
-						y >= 0 && y <= image_size) {
-		img_no = 3;
-		printf("Image 3\n");
-	} else if (x >= 0 && x <= image_size && y >= image_size + 1 &&
-							y <= image_size * 2) {
-		img_no = 4;
-		printf("Image 4\n");
-	} else if (x >= image_size + 1 && x <= image_size * 2 &&
-				y >= image_size + 1 && y <= image_size * 2) {
-		img_no = 5;
-		printf("Image 5\n");
-	} else if (x >= image_size * 2 + 1 && x <= image_size * 3 &&
-				y >= image_size + 1 && y <= image_size * 2) {
-		img_no = 6;
-		printf("Image 6\n");
-	} else if (x >= 0 && x <= image_size && y >= image_size * 2 + 1 &&
-							y <= image_size * 3) {
-		img_no = 7;
-		printf("Image 7\n");
-	} else if (x >= image_size + 1 && x <= image_size * 2 &&
-			y >= image_size * 2 + 1 && y <= image_size * 3) {
-		img_no = 8;
-		printf("Image 8\n");
-	} else if (x >= image_size * 2 + 1 && x <= image_size * 3 &&
-			y >= image_size * 2 + 1 && y <= image_size * 3) {
-		img_no = 9;
-		printf("Image 9\n");
-	}
-
-	return img_no;
-}
-
-static void lookup_image(ClutterActor *stage, int img_no)
-{
-	char *image;
-
-	if (img_no <= loaded_images) {
-		image = g_ptr_array_index(files,
-					array_pos - loaded_images + img_no - 1);
-		lookup_video(stage, image);
-	} else {
-		printf("No image at (%d)\n", img_no);
-	}
-}
- 
-/* Lookup the clicked image in the .movie-list file */
-static void lookup_video(ClutterActor *stage, char *actor)
-{
-	char string[512];
-	char image[120];
-	char movie[120];
-	char cmd[120];
-	char args[120] = "\0";
-	static FILE *fp;
-
-	printf("Opening movie list: (%s)\n", movie_list);
-	if (!(fp = fopen(movie_list, "r"))) {
-		printf("Can't open movie list: (%s)\n", movie_list);
-		exit(-1);
-	}
-	
-	while (fgets(string, 512, fp)) {
-		sscanf(string, "%119[^|]|%119[^|]|%119[^|]|%119[^\n]",
-						image, movie, cmd, args);
-		if (strcmp(actor, image) == 0) {
-			fclose(fp);
-			build_exec_cmd(cmd, args, movie);
-			return;
-		}
-
-		args[0] = '\0';
-	}
-
-	/* If we get to here, we didn't find a video */
-	fclose(fp);
-	printf("No video for (%s)\n", actor);
-	no_video_notice(stage);
-}
-
-/* Build the command and arguments to be exec'd */
-static void build_exec_cmd(char *cmd, char *args, char *movie)
-{
-	char video_paths[1024] = "\0";
-	char buf[512] = "\0";
-	gchar **argv = NULL;
-	gchar **videos = NULL;
-	
-	/* Build up a string that will be parsed into an argument list */
-	strcpy(buf, cmd);
-	strcat(buf, " ");
-	strcat(buf, args);
-	strcat(buf, " ");
-
-	/* Cater for multiple space seperated video paths to be specified */  
-	if (strstr(movie, " ")) {
-		videos = g_strsplit(movie, " ", -1);
-		while (*videos != NULL) {
-			/* 
- 			 * Cater for either absolute or relative paths 
- 			 * for videos. 
- 			 */
-			if (*videos[0] != '/') {
-				strcat(video_paths, movie_base_path);
-				strcat(video_paths, *videos++);
-			} else {
-				strcat(video_paths, *videos++);
-			}
-
-			strcat(video_paths, " ");
-		}
-	} else {
-		if (movie[0] == '/') {
-			strcpy(video_paths, movie);
-		} else {
-			strcpy(video_paths, movie_base_path);
-			strcat(video_paths, movie);
-		}
-	}
-
-	strcat(buf, video_paths);
-	/* Split buf up into a vector array for passing to execvp */
-	g_shell_parse_argv(buf, NULL, &argv, NULL);
-
-	printf("Playing: (%s)\n", video_paths);
-	printf("execing: %s %s %s\n", cmd, args, video_paths);
-	play_video(argv);
-}
-
-/* fork/exec the movie */
-static void play_video(gchar **argv)
-{
-	pid_t pid;
-
-	pid = fork();
-	if (pid > 0) {
-		/* parent */
-		g_strfreev(argv);
-	} else if (pid == 0) {
-		/* child */
-		execvp(argv[0], argv);
-	}
-}
-
-/* Display a "No Video" message for images with no video yet */
-static void no_video_notice(ClutterActor *stage)
-{
-	ClutterColor actor_color = { 0xff, 0xff, 0xff, 0xff };
-
-	label = clutter_text_new_full("Sans 32", "No Video", &actor_color);
-	clutter_actor_set_position(label, image_size,
-						image_size + 0.5 * image_size);
-	clutter_container_add_actor(CLUTTER_CONTAINER(stage), label);
-	clutter_actor_show(label);
-}
-
 /* Set the path for the movie-list mapping file */
 static void get_movie_list_path(char *home)
 {
@@ -573,44 +594,22 @@ static void get_movie_list_path(char *home)
 static void set_dimensions(char *size)
 {
 	window_size = atoi(size);
-		
-	if (window_size < 300 || window_size % 300 != 0) 
+
+	if (window_size < 300 || window_size % 300 != 0)
 		display_usage();
 
 	image_size = window_size / ROW_SIZE;
 
 	printf("Setting window size to (%dx%d)\n", window_size, window_size);
-	printf("Setting image size to  (%dx%d)\n", image_size, image_size); 
-}
-
-/* Display a help/usage summary */
-static void display_usage()
-{
-	printf("\nUsage: sodium image_directory size [video_directory]\n\n");
-	printf("Where image_directory is the path to the location of the ");
-	printf("images.\n\n");
-	printf("size is the size of the window to display in pixels. It ");
-	printf("must be at least 300 and also be a multiple of 300.\n\n");
-	printf("video_directory is an optional directory where videos are ");
-	printf("stored. Videos listed in the movie-list file should be ");
-	printf("given relative to this path.\n");
-
-	exit(-1);
-}
-
-/* Reap child pids */
-static void reaper(int signo)
-{
-	int status;
-
-	wait(&status);
+	printf("Setting image size to  (%dx%d)\n", image_size, image_size);
 }
 
 int main(int argc, char *argv[])
 {
 	ClutterActor *stage;
 	ClutterColor stage_clr = { 0x00, 0x00, 0x00, 0xff };
-	const gchar *stage_title = { "sodium - DVD Cover Art Viewer / Player" };
+	const gchar *stage_title =
+				{ "sodium - DVD Cover Art Viewer / Player" };
 	struct sigaction action;
 	char *e_animation;
 
@@ -633,7 +632,7 @@ int main(int argc, char *argv[])
 	if (argc == 4) {
 		strncpy(movie_base_path, argv[3], 120);
 		strcat(movie_base_path, "/");
-	} 
+	}
 
 	strncpy(image_path, argv[1], 240);
 	get_movie_list_path(getenv("HOME"));

ac000/sodium

commit a681183d0c57d000a3767fefb9eb8aa68fc2b295

Add the ability to sort the movies by runtime
Sometimes I like to quickly find a shorter length movie, by adding an
optional runtime field on the end of the entries in the movie mapping
file, this can be used to sort movies in order of runtime.

An entry could now look like

    taken.jpg|action/Taken.mkv|vlc|--fullscreen|5375

The movie length in seconds has been added and pressing the 's' key in
soidum will now cycle between sorting the movies by name (default) and
runtime in ascending order. Entries without a runtime are treated as 0.
There is a tools/add-runtimes.sh script to help in adding runtimes to an
existing mapping file.

To faciliate this rather than simply storing the image names in an
array we now store structures containing the image name and runtime.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

sodium.c
@@ -61,6 +61,14 @@
 		printf("%s: " fmt, __func__, ##__VA_ARGS__); \
 	} while (0)
 
+struct movie_info {
+	char *img_name;
+	int runtime;
+};
+
+/* Movie sort types */
+enum { SM_NAME, SM_RUNTIME };
+
 static bool debug;		/* Default to no debug output */
 static bool animation = true;	/* Animation default to enabled */
 /* label to display when an image is clicked that has no associated video */
@@ -77,6 +85,7 @@ static char *movie_list;	/* Path to movie-list mapping file */
 static char movie_base_path[PATH_MAX];
 static int window_size;	/* Size of the window */
 static int image_size;	/* Size of the image */
+static int sort_mode = SM_NAME;
 
 /* Display a help/usage summary */
 static void display_usage(void)
@@ -91,6 +100,14 @@ static void display_usage(void)
 	printf("given relative to this path.\n");
 
 	exit(EXIT_FAILURE);
+}
+
+static void free_movie_info(gpointer data)
+{
+	struct movie_info *mi = data;
+
+	free(mi->img_name);
+	free(mi);
 }
 
 /* Reap child pids */
@@ -130,18 +147,23 @@ static void reset_image(ClutterActor *actor)
 	clutter_actor_set_rotation_angle(actor, CLUTTER_Y_AXIS, 0.0);
 }
 
-/* Function to help sort the files list array */
-static int compare_string(const void *p1, const void *p2)
+/* Function to help sort the images by movie name */
+static int compare_names(const struct movie_info **mi1,
+			 const struct movie_info **mi2)
 {
-	/*
-	 * Function used from the qsort(3) man page
-	 *
-	 * The actual arguments to this function are "pointers to
-	 * pointers to char", but strcmp(3) arguments are "pointers
-	 * to char", hence the following cast plus dereference
-	 */
+	return strcmp((*mi1)->img_name, (*mi2)->img_name);
+}
 
-	return strcmp(*(char * const *)p1, *(char * const *)p2);
+/* Function to help sort the images by movie runtime in increasing length */
+static int compare_runtimes(const struct movie_info **mi1,
+			    const struct movie_info **mi2)
+{
+	if ((*mi1)->runtime < (*mi2)->runtime)
+		return -1;
+	else if ((*mi1)->runtime > (*mi2)->runtime)
+		return 1;
+	else
+		return 0;
 }
 
 /* Check to see if the file is a valid image */
@@ -162,7 +184,6 @@ static void process_directory(const gchar *name)
 {
 	DIR *dir;
 	struct dirent *entry;
-	char *fname;
 
 	dir = opendir(name);
 	if (!dir) {
@@ -173,21 +194,58 @@ static void process_directory(const gchar *name)
 	pr_debug("Opening directory: %s\n", name);
 	chdir(name);
 
-	files = g_ptr_array_new();
+	files = g_ptr_array_new_with_free_func(
+			(GDestroyNotify)free_movie_info);
 	while ((entry = readdir(dir)) != NULL) {
 		if (strcmp(entry->d_name, ".") == 0 ||
 		    strcmp(entry->d_name, "..") == 0)
 			continue;
 		if (is_supported_img(entry->d_name)) {
-			fname = g_strdup(entry->d_name);
-			pr_debug("Adding image %s to list\n", fname);
-			g_ptr_array_add(files, (gpointer)fname);
+			struct movie_info *mi;
+
+			mi = malloc(sizeof(struct movie_info));
+			mi->img_name = strdup(entry->d_name);
+			mi->runtime = 0;
+			pr_debug("Adding image %s to list\n", mi->img_name);
+			g_ptr_array_add(files, mi);
 			nfiles++;
 		}
 	}
 	closedir(dir);
 
-	g_ptr_array_sort(files, compare_string);
+	g_ptr_array_sort(files, (GCompareFunc)compare_names);
+}
+
+static void set_runtimes(void)
+{
+	char buf[BUF_SIZE];
+	FILE *fp;
+
+	fp = fopen(movie_list, "re");
+	if (!fp) {
+		fprintf(stderr, "Can't open movie list: (%s)\n", movie_list);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(buf, BUF_SIZE, fp)) {
+		char **fields = g_strsplit(buf, "|", 0);
+		int i;
+
+		if (!fields[4])
+			goto next;
+
+		for (i = 0; i < nfiles; i++) {
+			struct movie_info *mi = g_ptr_array_index(files, i);
+
+			if (strcmp(fields[0], mi->img_name) == 0) {
+				mi->runtime = atoi(fields[4]);
+				break;
+			}
+		}
+next:
+		g_strfreev(fields);
+	}
+	fclose(fp);
 }
 
 static void hide_label(void)
@@ -319,9 +377,10 @@ out:
 static void lookup_image(ClutterActor *stage, int img_no)
 {
 	if (img_no <= loaded_images) {
-		char *image = g_ptr_array_index(files,
+		struct movie_info *mi = g_ptr_array_index(files,
 				array_pos - loaded_images + img_no - 1);
-		lookup_video(stage, image);
+
+		lookup_video(stage, mi->img_name);
 	} else {
 		pr_debug("No image at (%d)\n", img_no);
 	}
@@ -393,11 +452,10 @@ static void load_images(ClutterActor *stage, int direction)
 		GdkPixbuf *pixbuf;
 		ClutterContent *image;
 		ClutterActor *ibox;
+		struct movie_info *mi = g_ptr_array_index(files, i);
 
-		pr_debug("Loading image: %s\n",
-			(char *)g_ptr_array_index(files, i));
-		pixbuf = gdk_pixbuf_new_from_file(
-				(char *)g_ptr_array_index(files, i), NULL);
+		pr_debug("Loading image: %s\n", mi->img_name);
+		pixbuf = gdk_pixbuf_new_from_file(mi->img_name, NULL);
 		image = clutter_image_new();
 		clutter_image_set_data(CLUTTER_IMAGE(image),
 				gdk_pixbuf_get_pixels(pixbuf),
@@ -495,6 +553,22 @@ static gboolean input_events_cb(ClutterActor *stage, ClutterEvent *event,
 				pr_debug("No image at (%d)\n", img_no);
 			}
 			break;
+		case CLUTTER_s: {
+			int (*cmp_func)(const struct movie_info **mi1,
+					const struct movie_info **mi2);
+
+			if (sort_mode == SM_NAME) {
+				cmp_func = compare_runtimes;
+				sort_mode = SM_RUNTIME;
+			} else {
+				cmp_func = compare_names;
+				sort_mode = SM_NAME;
+			}
+			g_ptr_array_sort(files, (GCompareFunc)cmp_func);
+			array_pos -= GRID_SIZE;
+			load_images(stage, FWD);
+			break;
+		}
 		case CLUTTER_Escape:
 		case CLUTTER_q:
 		case 269025110: /* Exit button on Hauppauge Nova-T remote */
@@ -650,6 +724,7 @@ int main(int argc, char *argv[])
 	clutter_actor_show(stage);
 
 	process_directory(image_path);
+	set_runtimes();
 	load_images(stage, FWD);
 
 	create_no_video_label(stage);
@@ -660,6 +735,7 @@ int main(int argc, char *argv[])
 	clutter_main();
 
 	free(movie_list);
+	g_ptr_array_free(files, TRUE);
 
 	exit(EXIT_SUCCESS);
 }

README
@@ -31,6 +31,7 @@ You can navigate via the following:
 	End				Last Page
 	c				Toggle cursor on/off
 	1 to 9				Select Video
+	s				Sort mode
 
 You can also scroll with the mouse wheel. NOTE: Mouse scroll events are limited
 to 1 scroll event every 750ms to prevent over scrolling on devices such as
@@ -49,12 +50,15 @@ names and the player to use and the arguments to pass to the player.
 It takes the form of a pipe (|) seperated fields flat file, e.g one entry
 would look like:
 
-	taken.jpg|action/Taken.mkv|xine|-f
+	taken.jpg|action/Taken.mkv|vlc|--fullscreen|6453
 
 taken.jpg 		is the name of image shown in sodium
 action/Taken.mkv 	is the relative path to the video
-xine			is the command to lauch to play the video
--f			is the arguments to pass to the video player
+vlc			is the command to lauch to play the video
+--fullscreen		is the arguments to pass to the video player
+6453			is the movie runtime in seconds
+
+The last field (runtime) is optional.
 
 When specifing a relative path you have to pass the optional /path/to/videos
 argument that is the top level directory where the videos are held. NOTE: you
@@ -77,6 +81,13 @@ If you have a box set of a series for example. You can do something like
 
 which would bring up a nautilus window showing the contents of the Season_4
 directory.
+
+If you specify the optional runtime field then when pressing 's' sodium will
+cycle between sorting by movie name (default) and sorting by runtime in
+ascending order. Entries with no runtime field will be treated as 0.
+
+There is a tools/add-runtimes.sh script to help in adding runtime entries to
+an existing mapping file.
 
 The mapping file should be called movie-list and placed under
 ~/.config/sodium/

sodium.1
@@ -1,4 +1,4 @@
-.TH SODIUM 1 "March 23, 2012"
+.TH SODIUM 1 "Jun 8, 2015" "" "General Commands Manual"
 .SH NAME
 sodium - DVD cover art viewer & movie launcher
 
@@ -35,17 +35,21 @@ You can optionally create a file called \fBmovie-list\fR under
 and takes the form:-
 
 .RS
-image|movie|player|args
+image|movie|player|args[|runtime]
 .RE
 
 e.g
 
 .RS
-taken.jpg|action/Taken.mkv|totem|--fullscreen
+taken.jpg|action/Taken.mkv|vlc|--fullscreen|5375
 .RE
 
 The movie path can be relative to the video path if given at runtime. Or it
-can be absolute. You can specify multiple paths in there.
+can be absolute. You can specify multiple space separated paths in there. See
+the \fBREADME\fR for more details.
+
+The runtime field is optional and can be used in sorting the movies in runtime
+order.
 
 .SH SOURCE
 Source available at http://github.com/ac000/sodium under the GNU GPLv2

tools/add-runtimes.sh
@@ -1,0 +1,31 @@
+#!/bin/sh
+#
+# Script to take a movies list and add an extra field on the end that
+# contains the length of the movie in seconds.
+#
+# It uses the ffprobe utility from ffmpeg to get the duration.
+#
+# This just outputs to stdout
+
+IFS="|"
+
+if [ $# -ne 2 ]; then
+	echo "Usage: add-runtimes.sh <movies list> <movies dir>"
+	exit 1
+fi
+
+cd $2
+
+while read -a movie
+do
+	echo -n "${movie[0]}|${movie[1]}|${movie[2]}|${movie[3]}"
+	runtime=$(ffprobe -i ${movie[1]} -show_entries format=duration -v quiet -of csv="p=0")
+	if [ $? -ne 0 ]; then
+		echo
+	else
+		echo -n "|"
+		expr ${runtime} : '\([0-9]*\)' + 1
+	fi
+done < $1
+
+exit 0

commit 12c283580217a30b8377c7887d9ea8e02d00af44

Fix selecting pf movies by number keys
You should be able to select a movie to play by pressing 1..9, however
this was broken by commit a681183 ("Add the ability to sort the movies
by runtime").

That commit changed what we store in the files GPtrArray, rather than
simply storing image names we now store structs and the
CLUTTER_1...CLUTTER_9 case selection failed to be updated for this new
situation.

The fix is simple, we just call lookup_image() as we do when selecting
a movie with the mouse.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

sodium.c
@@ -3,7 +3,7 @@
  *
  * DVD cover art viewer / player
  *
- * Copyright (C) 2008 - 2015	Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (C) 2008 - 2016	Andrew Clayton <andrew@digital-domain.net>
  *
  * License: GPLv2. See COPYING
  *
@@ -539,14 +539,10 @@ static gboolean input_events_cb(ClutterActor *stage, ClutterEvent *event,
 		}
 		case CLUTTER_1...CLUTTER_9:
 			img_no = sym - 48; /* 1 is sym(49) */
-			if (img_no <= loaded_images) {
-				char *image = g_ptr_array_index(files,
-					array_pos - loaded_images +
-					img_no - 1);
-				lookup_video(stage, image);
-			} else {
+			if (img_no <= loaded_images)
+				lookup_image(stage, img_no);
+			else
 				pr_debug("No image at (%d)\n", img_no);
-			}
 			break;
 		case CLUTTER_s: {
 			int (*cmp_func)(const struct movie_info **mi1,